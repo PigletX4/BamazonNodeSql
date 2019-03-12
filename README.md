13481d65aa0d9ccc91dfab83cf5cf52a5c1841df
Auri <aurihrp@hotmail.com>
2019-03-12 05:46:55 -0700

    Final version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # BamazonNodeSql
+
+Use: bamazoncustomer.js is a node program that can show a table of products, do sales math, and update the databases product stock.
+
+Functionality: bamazoncustomer.js automatically prompts for a product ID and then a quantity, and will only fulfil order if the quantity isn't higher than the current stock.
+
+For easy visual help check out the Video Demonstration file.
+
+Author: Auri H Robbins-Phillips
---
+++ bamazoncustomer.js
@@ -0,0 +1,102 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Youreprettygood1!",
+  database: "bamazon"
+});
+
+
+connection.connect(function (err) {
+  if (err) throw err;
+  showTable();
+});
+
+function showTable() {
+    connection.query('SELECT * FROM bamazon.products;', function(err, res){
+        if (err) throw err;
+        console.table(res);
+        startUp();
+    });
+};
+
+function startUp() {
+    connection.query("SELECT * FROM bamazon.products", function(err, results) {
+       
+        if (err) throw err;
+
+    inquirer.prompt([{
+        name: "purchaseItem",
+        type: "input",
+        message: "Please choose an item by id!"
+    },
+    {
+        name: "amount",
+        type: "input",
+        messages: "How many would you like?"
+    }
+    ]).then(function (answer) {
+        var itemChoice = answer.purchaseItem;
+        var itemAmount = answer.amount;
+        var currentId;
+        
+        for(var e = 0; e < 10; e++){
+            
+            var currentId = results[e].item_id;
+            var currentStock = results[e].stock_quantity;
+            var itemPrice = results[e].price;
+            var newStock = currentStock - itemAmount;
+            var purchaseTotal = (itemPrice * itemAmount)
+
+
+            if(itemChoice == currentId){
+
+                if(currentStock < itemAmount){
+                    console.log('Sorry, we currently do not have enough in stock!')
+                    connection.end();
+                    return
+
+                    
+                }
+                else{
+                    updateStock(newStock, itemChoice, purchaseTotal);
+                    return
+                }
+                
+            }
+            else{
+                continue
+            }
+
+        }
+        console.log("No such product.")
+        connection.end();
+
+
+
+    });
+  });
+}
+
+function updateStock(newStock, itemChoice, purchaseTotal){
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: newStock
+          },
+          {
+            item_id: itemChoice
+          }
+        ],
+        function(error) {
+          if (error) throw err;
+          console.log('Thank you for your purchase, your total is $' +purchaseTotal)
+          connection.end();
+          
+        }
+    );
+}
